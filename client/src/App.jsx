d95003b4b27a07dd85aa4297e0f6df4e4abf8aa7
Awais Murtaza <awaisfirstwork@gmail.com>
2024-07-23 08:09:23 +0500

    Signup Route and client is completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Button } from "./components/ui/button";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Auth from "./pages/auth";
+import Chat from './pages/chat';
+import Profile from './pages/profile';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<Auth/>}/>
+        <Route path="/chat" element={<Chat/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+
+
+        <Route path="*" element={<Navigate to="/auth" />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/utils/constants.js
@@ -0,0 +1,6 @@
+
+export const HOST = import.meta.env.VITE_SERVER_URL || "http://localhost:8767";
+
+export const AUTH_ROUTES = "api/auth";
+
+export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
